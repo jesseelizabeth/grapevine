36d413d66cac1d9576ba00bf865fdddfb8e7781b
Jesse Evans <jesseelizabeth1@gmail.com>
2019-07-17 10:26:16 -0400

    file setup

---
+++ client/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="nav-wrapper teal accent-4">
+          <div className="container">
+            <div className="brand-logo">Grapevine</div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
