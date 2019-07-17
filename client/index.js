21a60484ea0923ef179b7ec3fe98b70cee23eaee
Jesse Evans <jesseelizabeth1@gmail.com>
2019-07-17 14:30:26 -0400

    Apollo client setup

--- client/index.js
+++ client/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import history from './history';
-import store from './store';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
 
+// Apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+});
+
 ReactDOM.render(
-  <Provider store={store}>
+  <ApolloProvider client={client}>
     <Router history={history}>
       <App />
     </Router>
-  </Provider>,
+  </ApolloProvider>,
   document.getElementById('app')
 );
